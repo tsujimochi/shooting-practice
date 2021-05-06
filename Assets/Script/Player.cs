3b18e18cab489aab86ccb82b25ed5b4fe6e62a09
tsujimochi <tsujimochi@nantonaku-rt58.com>
2021-05-06 16:42:53 +0900

    自機操作対応

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 移動スピード
+    public float speed = 5;
+
+    void Update ()
+    {
+		// 右・左
+		float x = Input.GetAxisRaw ("Horizontal");
+		
+		// 上・下
+		float y = Input.GetAxisRaw ("Vertical");
+		
+		// 移動する向きを求める
+		Vector2 direction = new Vector2 (x, y).normalized;
+		
+		// 移動する向きとスピードを代入する
+		GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
