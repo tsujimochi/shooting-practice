9827bc0b0a7f4460d9f6a8fd1b08000f96d9e7cd
tsujimochi <tsujimochi@nantonaku-rt58.com>
2021-05-06 17:04:16 +0900

    自機から弾を発射

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int speed = 10;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.right.normalized * speed;
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,6 +7,24 @@ public class Player : MonoBehaviour
     // 移動スピード
     public float speed = 5;
 
+    // 弾速度
+    public float bulletSpeed = 0.5f;
+
+    // PlayerBulletプレハブ
+    public GameObject bullet;
+
+    // Startメソッドをコルーチンとして呼び出す
+    IEnumerator Start ()
+    {
+        while (true) {
+            // 弾をプレイヤーと同じ位置/角度で作成
+            Instantiate (bullet, transform.position, transform.rotation);
+            // 弾発射            
+            yield return new WaitForSeconds(bulletSpeed);
+        }
+    }
+
+
     void Update ()
     {
 		// 右・左
